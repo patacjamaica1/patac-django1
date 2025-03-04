3f2a0714977225fe79f0cd86035cb073f1dc9150
JAM <126861194+patacjamaica1@users.noreply.github.com>
2025-03-04 23:24:40 +0800

    Initial commit for react

---
+++ post/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title  # Fixed typo from 'titles' to 'title'
